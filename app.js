e67d582283ebaca0edde9da805129e7f1ace1d5e
Fernando Carrizo <fer-nando-08@hotmail.com>
2022-09-06 16:49:32 -0300

    Ejercicio 1

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const countries = require('./countries.json');
  * @returns {Object}
  */
 function getCountryByIso3(isoCode){
-    //Complete with your code
+    const country = countries.find(data => data.iso3 === isoCode);
+    return country;
 }
 
 /**
